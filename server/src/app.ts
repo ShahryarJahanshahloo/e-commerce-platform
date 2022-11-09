5f03c58026fef5863473ec88ebe0afa3e127bd4d
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2022-11-09 16:56:31 +0330

    removed nextjs default files

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express'
+import cors from 'cors'
 import router from './routes/index'
 
 const app: Express = express()
 
 app.use(express.json())
+app.use(cors())
 app.use(router)
 
 export default app
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -2,4 +2,8 @@ import express, { Router } from 'express'
 
 const router: Router = express.Router()
 
+router.get('/ping', (req, res) => {
+  res.send({ message: 'pong' })
+})
+
 export default router
