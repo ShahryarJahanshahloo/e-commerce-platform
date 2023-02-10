1b46fa13dde715e2d8f3c00e02875383f98d307a
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2023-02-10 12:24:28 +0330

    added color variables

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,28 +1,15 @@
-import { useEffect } from 'react'
-import { useAppSelector } from '../utils/store'
-import { fetchCart } from '../services/cart/cart.thunks'
-import useThunk from '../hooks/useThunk'
-import Loading from '../components/Loading/Loading'
+import s from '../styles/home.module.scss'
 
 const HomePage = () => {
-  const cart = useAppSelector(state => state.cart.data)
-  const { isLoading, sendRequest } = useThunk(fetchCart)
-
-  useEffect(() => {
-    sendRequest()
-  }, [])
-
   return (
     <>
-      <div>
-        {isLoading ? (
-          <Loading />
-        ) : (
-          cart?.map(item => (
-            <div key={item.storageItem}>{item.productName}</div>
-          ))
-        )}
-      </div>
+      <h1 className={s.mamad}>mamad</h1>
+      <div>goomba</div>
+      <div>goomba</div>
+      <div>goomba</div>
+      <div>goomba</div>
+      <div>goomba</div>
+      <div>goomba</div>
     </>
   )
 }
