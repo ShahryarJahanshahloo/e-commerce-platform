07aa161087b1dba982769a95b1a5b0ff4efb0243
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2023-02-15 08:42:05 +0330

    cleared home page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,18 +1,9 @@
 import s from '../styles/home.module.scss'
-import { toast } from 'react-toastify'
 
 const HomePage = () => {
-  const notify = () => toast.success('اضافه شد')
   return (
     <>
-      <h1 className={s.mamad}>mamad</h1>
-      <h2 onClick={notify}>Notify!</h2>
-      <div>goomba</div>
-      <div>goomba</div>
-      <div>goomba</div>
-      <div>goomba</div>
-      <div>goomba</div>
-      <div>goomba</div>
+      <h1>HomePage</h1>
     </>
   )
 }
