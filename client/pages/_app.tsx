3925fad00fe155102459b6ee54f6d85ed9430a7f
shahryar.jahanshahloo <shahryar.jahanshahloo@gmail.com>
2023-02-19 18:02:18 +0330

    removed tailwind

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,4 +1,4 @@
-import '../styles/globals.css'
+import '../styles/globals.scss'
 import 'react-toastify/dist/ReactToastify.css'
 import type { ReactElement, ReactNode } from 'react'
 import type { NextPage } from 'next'
